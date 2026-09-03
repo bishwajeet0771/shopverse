9252f289757a15ab2ec746ec5158c44570997967
bishwajeet0771 <biswajit.kjr55@gmail.com>
2026-09-03 14:40:21 +0530

    Updated the terrafirm rds modules main.

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -32,15 +32,15 @@ resource "aws_security_group" "rds" {
 }
 
 resource "aws_security_group_rule" "rds_ingress" {
-  for_each = toset(var.allowed_security_group_ids)
+  count = length(var.allowed_security_group_ids)
 
   type                     = "ingress"
   from_port                = 5432
   to_port                  = 5432
   protocol                 = "tcp"
   security_group_id        = aws_security_group.rds.id
-  source_security_group_id = each.value
-  description               = "Postgres from EKS nodes"
+  source_security_group_id = var.allowed_security_group_ids[count.index]
+  description              = "Postgres from EKS nodes"
 }
 
 # ──────────────────────────────────────────────
